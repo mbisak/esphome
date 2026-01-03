d0b111290da152b241a93965c092ff7aaa19e789
mbisak <80639683+mbisak@users.noreply.github.com>
2026-01-03 22:15:27 +0100

    Update output.py

--- components/ac_dimmer_32/output.py
+++ components/ac_dimmer_32/output.py
@@ -7,7 +7,7 @@ from esphome.const import CONF_ID, CONF_METHOD, CONF_MIN_POWER
 CODEOWNERS = ["@glmnet"]
 
 ac_dimmer_ns = cg.esphome_ns.namespace("ac_dimmer_32")
-AcDimmer = ac_dimmer_ns.class_("AcDimmer_32", output.FloatOutput, cg.Component)
+AcDimmer_32 = ac_dimmer_ns.class_("AcDimmer_32", output.FloatOutput, cg.Component)
 
 DimMethod = ac_dimmer_ns.enum("DimMethod")
 DIM_METHODS = {
